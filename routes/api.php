585af66454441f5c3f7928a1d11af0d46167aeff
kukusha <yeabsira0514@gmail.com>
2026-07-05 09:01:21 +0300

    feat: install Laravel Sanctum and configure API authentication routes and migrations

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DeviceEnrollmentController;
+
+Route::group([
+    'prefix' => 'admin/auth'
+], function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:api');
+    Route::post('refresh', [AuthController::class, 'refresh'])->middleware('auth:api');
+    Route::get('me', [AuthController::class, 'me'])->middleware('auth:api');
+});
+
+Route::post('/enroll', [DeviceEnrollmentController::class, 'enroll']);
