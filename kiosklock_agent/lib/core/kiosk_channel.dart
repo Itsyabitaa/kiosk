085ac74d72c6274fe8608ce21b9b410e9eae7595
kukusha <yeabsira0514@gmail.com>
2026-07-05 09:20:30 +0300

    feat: implement KioskChannel for platform-native kiosk mode and device restriction management

---
+++ kiosklock_agent/lib/core/kiosk_channel.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/services.dart';
+
+class KioskChannel {
+  static const _channel = MethodChannel('com.kiosklock/kiosk');
+
+  static Future<bool> lockToApp(String packageName) async {
+    final result = await _channel.invokeMethod<bool>('lockToApp', {'package': packageName});
+    return result ?? false;
+  }
+
+  static Future<bool> unlock() async {
+    final result = await _channel.invokeMethod<bool>('unlock');
+    return result ?? false;
+  }
+
+  static Future<bool> applyRestrictions(Map<String, dynamic> restrictions) async {
+    final result = await _channel.invokeMethod<bool>('applyRestrictions', restrictions);
+    return result ?? false;
+  }
+
+  static Future<Map<String, dynamic>> getDeviceState() async {
+    final result = await _channel.invokeMapMethod<String, dynamic>('getDeviceState');
+    return result ?? {};
+  }
+}
